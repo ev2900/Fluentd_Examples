686122374e012924e599729bb9acfd1821343d84
Christopher Sharkey <ev2900@gmail.com>
2022-04-23 13:14:41 -0400

    updated the map IAM user script

--- Cloud9_Apache_Logs_OpenSearch/map_IAM_user.py
+++ Cloud9_Apache_Logs_OpenSearch/map_IAM_user.py
@@ -11,16 +11,22 @@ import json
 # --------------
 # Step 1 - Update this URL with your domain endpoint
 # --------------
-os_url = 'https://search-workshop-domain-atyey3n6ezpbugcxlk623l5ayi.us-east-1.es.amazonaws.com'
+os_url = 'https://search-fluentd-domain-qjskzejtju5tryntaw6s4wkbfa.us-east-1.es.amazonaws.com'
 
 # --------------
 # Step 2 - Update ARN of IAM role to map to OpenSearch all_access role
 # --------------
 
-iam_arn = ''
+iam_arn = 'arn:aws:iam::376390845435:user/Fluentd_User_OpenSearch'
 
 # --------------
-# Step 3
+# Step 3 - Map IAM user ARN to all_access role
 # --------------
+request_body = {
+	"backend_roles" : [ "all_access"],
+	"users" : [ '"' + iam_arn + '"', "OSMasterUser"]
+}	
 
+map_user_r = requests.put(os_url + '/_plugins/_security/api/rolesmapping/all_access', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(request_body))
 
+print(map_user_r.text)
