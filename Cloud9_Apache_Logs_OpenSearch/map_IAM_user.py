326527895a704ecf4979081cae0b4770d3c9e3b9
Christopher Sharkey <ev2900@gmail.com>
2022-04-23 09:36:39 -0400

    working on the IAM mapping

---
+++ Cloud9_Apache_Logs_OpenSearch/map_IAM_user.py
@@ -0,0 +1,26 @@
+# --------------
+# Prerequisite
+# --------------
+# If you have not already installed the requests package and/or the json package 
+# a. pip install requests
+# b. pip install json
+
+import requests
+import json
+
+# --------------
+# Step 1 - Update this URL with your domain endpoint
+# --------------
+os_url = 'https://search-workshop-domain-atyey3n6ezpbugcxlk623l5ayi.us-east-1.es.amazonaws.com'
+
+# --------------
+# Step 2 - Update ARN of IAM role to map to OpenSearch all_access role
+# --------------
+
+iam_arn = ''
+
+# --------------
+# Step 3
+# --------------
+
+
